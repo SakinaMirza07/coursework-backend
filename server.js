1c127a5bc154323e177908f1edfb110838e6e44d
Sakina Mirza <sakina34540@gmail.com>
2024-12-03 21:17:07 +0400

    Made the logger middleware to output all the requests comming on the console

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  const now = new Date().toISOString();
+  console.log(`[${now}] ${req.method} ${req.url}`);
+  next(); 
+});
+
 const MongoClient = require("mongodb").MongoClient;
 let db;
 
